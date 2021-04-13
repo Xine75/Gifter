e0645cd3368abfc8480034bd116d52f3b44e50a0
Xine <cjdoza@gmail.com>
2021-04-13 14:53:59 -0500

    PostSearch component made

---
+++ client/src/components/PostSearch.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { PostContext } from "../providers/PostProvider"
+
+export const PostSearch = () => {
+    const { setSearchTerms } = useContext(PostContext)
+
+    return (
+        <>
+            Search Posts:
+            <input type="text" className="input-wide"
+                onKeyUp={(e) => setSearchTerms(e.target.value)}
+                placeholder="Search for a Post" />
+
+        </>
+    )
+}
