b1c28869b1808baa66c2b0cd639e56f5494027bc
Xine <cjdoza@gmail.com>
2021-04-13 09:48:33 -0500

    post form

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import { PostProvider } from "./providers/PostProvider";
 import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 function App() {
   return (
     <div className="App">
       <PostProvider>
-        {/* <AwesomeNewPostComponent/> */}
+        <PostForm />
         <PostList />
       </PostProvider>
     </div>
--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -7,6 +7,9 @@ export const PostForm = () => {
     const history = useHistory();
 }
 
+const timestamp = Date.now()
+const postDate = new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).format(timestamp)
+
 
 //----------------Setting State ----------------
 
@@ -15,7 +18,7 @@ const [post, setPost] = useState({
     title: "",
     imageUrl: "",
     caption: "",
-    dateCreated: "",
+    dateCreated: postDate,
 })
 
 //-------------Saving User Input --------------
@@ -38,3 +41,32 @@ const handleClickSavePost = (e) => {
 
 //-------------------------JSX Add Post ----------------------
 
+return (
+    <>
+        <form className="postForm">
+            <h3 className="postForm__title">Add a Gif!</h3>
+
+            <br />
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="name">Gif Title:</label>
+                    <input type="text" id="title" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Title of Gif" value={post.title} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="name">Gif Url:</label>
+                    <input type="text" id="imageUrl" onChange={handleControlledInputChange} required className="form-control" placeholder="Paste Gif Url Here" value={post.imageUrl} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="name">Caption:</label>
+                    <input type="text" id="caption" onChange={handleControlledInputChange} required className="form-control" placeholder="Your Caption" value={post.caption} />
+                </div>
+            </fieldset>
+            <button className="btn btn-info" disabled={isLoading} onClick={handleClickSavePost}>Add Post</button>
+
+        </form>
+    </>
+)
